38ff6658b2a2c5020cf1b0b359f63f8dada16558
nullpointertong <auz.richardt@gmail.com>
2018-09-19 14:40:47 +1000

    First C file commit

---
+++ project.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/*Function Prototypes*/
+
+
+/*Structures*/
+/*Node Structure in Order to implment a linkedlist*/
+struct node {
+    struct node* nextp;
+};
+typedef struct node node_t;
+
+
+
+
+int main() {
+    node_t* linkedListp;
+    node_t* index_pointer;
+    linkedListp = (node_t*) malloc(sizeof (node_t));
+    free(linkedListp);
+    linkedListp = NULL;
+    return 0;
+}
+
