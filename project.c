20ca57ba7931971de54e32abda433630530c7dac
nullpointertong <auz.richardt@gmail.com>
2018-09-19 15:16:18 +1000

    Corrected

--- project.c
+++ project.c
@@ -22,7 +22,7 @@ int main() {
     linkedListp = (node_t*) malloc(sizeof (node_t));
     free(linkedListp);
     linkedListp = NULL;
-	printf("RICHARD GEY\N");
+	printf("Seb's Mother GEY\N");
     return 0;
 }
 
