a9446b7ab143d00c78c967147ad1d6faf6920a0d
nullpointertong <auz.richardt@gmail.com>
2018-09-26 03:59:27 +1000

    Basic Hashmap Display mapping for debugging created

--- project.c
+++ project.c
@@ -17,20 +17,25 @@ struct node {
 typedef struct node node_t;
 
 /*Function Prototypes*/
-void insert_hashmap(int *sampleData,map_t hashmap[],int * new_key);
+void insert_hashmap(int *employeeId,map_t hashmap[],int * new_key);
+void display_hashmap(int *employeeId,map_t hashmap[],int * new_key);
+
 
 int main() {
 	/* Assuming 10 is the maximum amount this hashmap can store
 	*/
 	int i;
-	int sampleData = 10;
+	int employeeId;
 	map_t hashmap[10];
 	for(i = 0 ;i<10 ; i++){
     hashmap[i].value = 0;
     hashmap[i].key = 0;
     }
-    int new_key = sampleData % 10;    
-    insert_hashmap(&sampleData, hashmap, &new_key);
+    printf("Enter Employee Id");
+    scanf("%d", &employeeId);
+    int new_key = employeeId % 10;    
+    insert_hashmap(&employeeId, hashmap, &new_key);
+    display_hashmap(&employeeId, hashmap, &new_key);
 /*
     node_t* linkedListp;
     node_t* index_pointer;
@@ -43,19 +48,31 @@ int main() {
 }
 
 
-void insert_hashmap(int *sampleData,map_t hashmap[], int *new_key)
+void insert_hashmap(int *employeeId,map_t hashmap[], int *new_key)
 {
 	/*Please Note Hashmap is implmented in the form h(K) = KmodN where 
 	N is number of elements in array and K is key*/
 	if(hashmap[*new_key].key == 0 && hashmap[*new_key].value == 0){
-	hashmap[*new_key].value = *sampleData; 
+	hashmap[*new_key].value = *employeeId; 
     hashmap[*new_key].key = *new_key;
     }
     else if(hashmap[*new_key].key != 0 && hashmap[*new_key].value != 0){
+    /*Linear Collision */
+
     	(*new_key)++;  
-    	return insert_hashmap(sampleData,hashmap,new_key);
+    	return insert_hashmap(employeeId,hashmap,new_key);
     }
-    
 }
+
+void display_hashmap(int *employeeId,map_t hashmap[],int * new_key)
+{
+	int i;
+	printf("Value Key\n");
+	for(i = 0 ; i<10 ; i++){
+		printf("%d %d\n",hashmap[i].key, hashmap[i].value);
+	}
+}
+
+    
 
 
