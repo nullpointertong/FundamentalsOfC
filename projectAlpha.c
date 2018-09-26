8c6bcce2ff9ed115652146d5e9ddfb3b97df4db7
sbeb98 <43396208+sbeb98@users.noreply.github.com>
2018-09-26 15:12:38 +1000

    Update projectAlpha.c

--- projectAlpha.c
+++ projectAlpha.c
@@ -1,5 +1,3 @@
-typedef struct map map_t;
-
 struct node {
     struct node* nextp;
     user_t user;
@@ -10,13 +8,38 @@ typedef struct node node_t;
 struct user {
     char username[15];
     char password[15];
+    char userID[5];
     account_t account[5];
 };
 typedef struct user user_t;
 
 
 struct account {
-	int accountNum;
+	char accountNum[10];
 	int accountValue;
 };
 typedef struct account account_t;
+
+/*Prototypes*****************************/
+node_t readFile(); 
+int writeFile(node_t linkList);
+int newUser(node_t linkList, int numUsers);  
+void accountMenu(node_t linkList);
+
+void addNewAccount(node_t linklist);
+void PayUser(node_t linkList, int amount);
+void TransferMoney(char accountNum1[],char accountNum2[],int amount);
+void WhitdrawMoney(char accountNum[],int amount);
+void DepositMoney(char accountNum[],int amount); 
+
+user_t* i;
+for (*i = Linkedlist.user; i != NULL; ++i)
+{
+	/* if(node == somecondition)
+ 	node.user.Accountname*/
+	if(i.user.accountID=accountID)
+	{
+	do some shit
+		return i*;
+	}
+}
