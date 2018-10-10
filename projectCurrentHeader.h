9d136cdd03343140ac3a4f1ef88dc724736b85e9
sbeb98 <43396208+sbeb98@users.noreply.github.com>
2018-10-10 17:14:06 +1100

    richard is little bitch

--- projectCurrentHeader.h
+++ projectCurrentHeader.h
@@ -41,19 +41,16 @@ typedef struct map map_t;
 /**************Prototypes*************************/
 node_t login(node_t linkList, node_t p, int debugFlag);
 char accountMenu();
-int addNewAccount(node_t linkList, node_t p, int debugFlag); /*Added userID*/
+int addNewAccount(node_t linkList, node_t p, int debugFlag);
 void TransferMoney(node_t p, node_t linkList, int debugFlag);
-node_t PayUser(node_t p, node_t linkList);
 void withdrawMoney(node_t p, node_t linkList, int debugFlag);
 int depositMoney(node_t p, node_t linkList, int debugFlag);
 node_t newNode(node_t linkList, node_t p);
 node_t newUser(node_t linkList, int *numUsers, node_t p, int debugFlag);
 int writeFile(node_t p, int* numUsers);
-void deleteAccount(node_t p);
-char startMenu();
 node_t userMenu(node_t linkList, node_t p, int *debugFlag, int *numUsers);
 void listAccounts(node_t p);
-char miniMenu(char name[], node_t p, node_t linkList);
+
 void insert_hashmap(int *employeeId, map_t hashmap[], int * new_key);
 void display_hashmap(int *employeeId, map_t hashmap[], int * new_key);
 int writeFileV3(node_t linkList, int* numUsers);
